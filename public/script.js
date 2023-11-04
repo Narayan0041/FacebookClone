3bcfb781b3028483f7e7846c1874314e56c8f03e
Shiv@9089 <yadavshiv0041@gmail.com>
2023-11-04 12:58:46 +0530

    complete Facebook clone template project

---
+++ public/script.js
@@ -0,0 +1,8 @@
+var darkBtn =document.querySelector(".dark-mode");
+
+darkBtn.onclick = function(){
+   darkBtn.classList.toggle("dark-btn-on");
+   document.body.classList.toggle("dark-theme");
+ 
+   
+}
